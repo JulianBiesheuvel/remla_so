002f5e2b961867564f10cb5f5af77512a9b6aae2
Johannes Koch <j.koch@student.tudelft.nl>
2022-05-19 18:05:11 +0200

    worked on DVC, versioning and mllint

--- code/get_data.py
+++ code/get_data.py
@@ -5,12 +5,12 @@ Currently the data already is here locally, so this step does nothing.
 import urllib.request
 import zipfile
 
+from . import conf
+
 # URL = '<URL>'
 URL = None
-
-EXTRACT_DIR = "dataset"
 
 if URL is not None:
     zip_path, _ = urllib.request.urlretrieve(URL)  # nosec
     with zipfile.ZipFile(zip_path, "r") as f:
-        f.extractall(EXTRACT_DIR)
+        f.extractall(conf.RAW_DATA_DIR)
