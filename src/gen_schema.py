44b93077382da7d5eed38a76d476acc6abc07cbb
Johannes Koch <j.koch@student.tudelft.nl>
2022-05-23 17:28:29 +0200

    add scrapy

---
+++ src/gen_schema.py
@@ -0,0 +1,23 @@
+""" Generates the tfdv schema from data """
+
+
+import os
+
+import pandas as pd
+import tensorflow_data_validation as tfdv
+
+from src import conf
+
+
+def main() -> None:
+    """
+    Generates the schema from a tsv file and stores the schema.
+    """
+    dataframe = pd.read_csv(os.path.join(conf.PROCESSED_DATA_DIR, "preprocessed.csv"))
+    stats = tfdv.generate_statistics_from_dataframe(dataframe)
+    schema = tfdv.infer_schema(statistics=stats)
+    tfdv.write_schema_text(schema, os.path.join(conf.DATA_STATS_DIR, "schema.txt"))  # type: ignore
+
+
+if __name__ == "__main__":
+    main()
--- src/validate.py
+++ src/validate.py
@@ -11,14 +11,25 @@ from sklearn.model_selection import train_test_split
 from src import conf, utils
 
 
+def gen_schema(
+    stats: tfdv.statistics_pb2.DatasetFeatureStatisticsList,
+) -> tfdv.schema_pb2.Schema:
+    schema = tfdv.infer_schema(statistics=stats)
+    tfdv.write_schema_text(schema, os.path.join(conf.DATA_STATS_DIR, "schema.txt"))  # type: ignore
+    return schema
+
+
 def gen_stats(
-    dataframe: pd.DataFrame, name: AnyStr
+    schema: tfdv.tfdv.schema_pb2.Schema, dataframe: pd.DataFrame, name: AnyStr
 ) -> Tuple[List[str], List[List[str]]]:
     """
     Generate tfdv stats for a dataframe and return the contents of the dataframe.
     """
     stats = tfdv.generate_statistics_from_dataframe(dataframe)
-    tfdv.write_stats_text(stats, os.path.join(conf.DATA_STATS_DIR, name + ".txt"))  # type: ignore
+    anomalies = tfdv.validate_statistics(stats, schema=schema)
+    tfdv.write_anomalies_text(anomalies, os.path.join(conf.DATA_STATS_DIR, f"anomalies_{name}.txt"))  # type: ignore
+    tfdv.write_stats_text(stats, os.path.join(conf.DATA_STATS_DIR, f"stats_{name}.txt"))  # type: ignore
+
     X, y = (
         dataframe["title"].to_numpy(),
         dataframe["tags"].apply(literal_eval).to_numpy(),
@@ -31,9 +42,11 @@ def main() -> None:
     Splits the raw processed data into training/validation and generates tfdv statistics.
     """
     dataframe = pd.read_csv(os.path.join(conf.PROCESSED_DATA_DIR, "preprocessed.csv"))
-    train, test = train_test_split(dataframe, random_state=42)
-    train = gen_stats(train, "train")
-    validation = gen_stats(test, "validation")
+    train, test = train_test_split(dataframe, test_size=0.3, random_state=42)
+    schema = tfdv.load_schema_text(os.path.join(conf.DATA_STATS_DIR, "schema.txt"))
+
+    train = gen_stats(schema, train, "train")
+    validation = gen_stats(schema, test, "validation")
 
     utils.store(
         train,
