354caa21e57960c5c289b1d54482f3f6ba600393
Johannes Koch <j.koch@student.tudelft.nl>
2022-06-08 01:36:33 +0200

    activate scraper pipeline

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -64,9 +64,9 @@ EXTENSIONS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'scraper.pipelines.PgPipeline': 300,
-# }
+ITEM_PIPELINES = {
+   'scraper.pipelines.PgPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
